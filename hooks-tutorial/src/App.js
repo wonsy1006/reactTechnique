24e9f2f644825c9388f283eb1820b6bd50851107
wonsy1006 <wonsy1006@gmail.com>
2022-10-06 17:01:17 +0900

    Commit before npm run eject

---
+++ hooks-tutorial/src/App.js
@@ -0,0 +1,10 @@
+// import Counter from "./Counter";
+// import { useState } from "react";
+// import Info from "./Info";
+import Average from "./Average";
+
+const App = () => {
+  return <Average />;
+};
+
+export default App;
